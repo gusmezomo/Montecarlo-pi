e25f8eadc412c94db5c555e887111a806f85b90d
gusmezomo <gusmezomo@gmail.com>
2026-05-26 19:58:45 -0300

    monte paralelo v2 openmp, ajustes

--- montecarlo-paralelo.c
+++ montecarlo-paralelo.c
@@ -17,22 +17,28 @@ int main() {
 
     srand(time(NULL));
 
-    #pragma omp parallel for reduction(+:circle_points) //schedule(static,1) - PARA DISTRUBUIR AS THREADS ALTERNADAMENTE
-    for (i = 0; i < (TOTAL_POINTS); i++) {
+    #pragma omp parallel reduction(+:circle_points) 
+    {
+        // cria uma seed diferente para cada thread usando o tempo atual + id da thread
+        unsigned int seed = time(NULL) + omp_get_thread_num();
 
-        //imprimir as 20 primeiras interaçoes
-        int THREADS = omp_get_thread_num();
-        if (i < 20) {
-            printf("Iteracao %d -> Thread %d\n", i, THREADS);
+        #pragma omp for schedule(static,1) //schedule(static,1) - PARA DISTRUBUIR AS THREADS ALTERNADAMENTE
+        for (i = 0; i < (TOTAL_POINTS); i++) {
+
+            //imprimir as 20 primeiras interaçoes
+            int thread_id = omp_get_thread_num();
+            if (i < 20) {
+                printf("Iteracao %d -> Thread %d\n", i, thread_id);
+            }
+
+            rand_x = (double)(rand_r(&seed) % (INTERVAL + 1)) / INTERVAL;
+            rand_y = (double)(rand_r(&seed) % (INTERVAL + 1)) / INTERVAL;
+
+            distancia_origem = rand_x * rand_x + rand_y * rand_y;
+
+            if (distancia_origem <= 1)
+                circle_points++;
         }
-
-        rand_x = (double)(rand() % (INTERVAL + 1)) / INTERVAL;
-        rand_y = (double)(rand() % (INTERVAL + 1)) / INTERVAL;
-
-        distancia_origem = rand_x * rand_x + rand_y * rand_y;
-
-        if (distancia_origem <= 1)
-            circle_points++;
     }
 
     pi = 4.0 * circle_points / TOTAL_POINTS;
