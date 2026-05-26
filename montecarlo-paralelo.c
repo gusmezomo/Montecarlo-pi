cd5fe3b3aafe9ea042443c9d64c716a2a1bb46dc
gusmezomo <gusmezomo@gmail.com>
2026-05-26 20:14:59 -0300

    portugues

--- montecarlo-paralelo.c
+++ montecarlo-paralelo.c
@@ -5,25 +5,25 @@
 #include <time.h>
 #include <omp.h>
 
-#define INTERVAL 10000
-#define TOTAL_POINTS 100000000
+#define INTERVALO 10000
+#define TOTAL_PONTOS 100000000
 
 int main() {
     int i;
     double rand_x, rand_y, distancia_origem, pi;
-    int circle_points = 0;
+    int pontos_circulo = 0;
 
     omp_set_num_threads(8);
 
     srand(time(NULL));
 
-    #pragma omp parallel reduction(+:circle_points) 
+    #pragma omp parallel reduction(+:pontos_circulo) 
     {
         // cria uma seed diferente para cada thread usando o tempo atual + id da thread
         unsigned int seed = time(NULL) + omp_get_thread_num();
 
         #pragma omp for schedule(static,1) //schedule(static,1) - PARA DISTRUBUIR AS THREADS ALTERNADAMENTE
-        for (i = 0; i < (TOTAL_POINTS); i++) {
+        for (i = 0; i < (TOTAL_PONTOS); i++) {
 
             //imprimir as 20 primeiras interaçoes
             int thread_id = omp_get_thread_num();
@@ -31,17 +31,17 @@ int main() {
                 printf("Iteracao %d -> Thread %d\n", i, thread_id);
             }
 
-            rand_x = (double)(rand_r(&seed) % (INTERVAL + 1)) / INTERVAL;
-            rand_y = (double)(rand_r(&seed) % (INTERVAL + 1)) / INTERVAL;
+            rand_x = (double)(rand_r(&seed) % (INTERVALO + 1)) / INTERVALO;
+            rand_y = (double)(rand_r(&seed) % (INTERVALO + 1)) / INTERVALO;
 
             distancia_origem = rand_x * rand_x + rand_y * rand_y;
 
             if (distancia_origem <= 1)
-                circle_points++;
+                pontos_circulo++;
         }
     }
 
-    pi = 4.0 * circle_points / TOTAL_POINTS;
+    pi = 4.0 * pontos_circulo / TOTAL_PONTOS;
 
     printf("Estimativa de PI = %f\n", pi);
     printf("Numero de threads gerados: %d\n", omp_get_max_threads());
--- montecarlo-sequencial.c
+++ montecarlo-sequencial.c
@@ -2,28 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define INTERVAL 10000
-#define TOTAL_POINTS 100000000
+#define INTERVALO 10000
+#define TOTAL_PONTOS 100000000
 
 int main() {
     int i;
     double rand_x, rand_y, distancia_origem, pi;
-    int circle_points = 0;
+    int pontos_circulo = 0;
 
     srand(time(NULL));
 
-    for (i = 0; i < (TOTAL_POINTS); i++) {
+    for (i = 0; i < (TOTAL_PONTOS); i++) {
 
-        rand_x = (double)(rand() % (INTERVAL + 1)) / INTERVAL;
-        rand_y = (double)(rand() % (INTERVAL + 1)) / INTERVAL;
+        rand_x = (double)(rand() % (INTERVALO + 1)) / INTERVALO;
+        rand_y = (double)(rand() % (INTERVALO + 1)) / INTERVALO;
 
         distancia_origem = rand_x * rand_x + rand_y * rand_y;
 
         if (distancia_origem <= 1)
-            circle_points++;
+            pontos_circulo++;
     }
 
-    pi = 4.0 * circle_points / TOTAL_POINTS;
+    pi = 4.0 * pontos_circulo / TOTAL_PONTOS;
 
     printf("Estimativa de PI = %f\n", pi);
 
